babcbeadbf45f9ab82097c33d9786cc4d4591a1b
aya-web6 <143331979+aya-web6@users.noreply.github.com>
2023-10-12 11:53:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var contactus=document.querySelector(".contactus");
+var products=document.querySelector(".products");
+
+
+contactus.addEventListener("click",function(){
+    products.style.display="block";
+})
